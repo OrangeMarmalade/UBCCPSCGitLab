e0c595da523b717d9eaaf287489197fc1480795c
Ziqin (Jimmy) Wang <jimmywangzq@gmail.com>
2014-09-26 10:35:25 -0700

    jimmy's bad commit

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -60,10 +60,7 @@ public class GitLab implements EntryPoint {
 		
 		flexTable.setText(0,0, "Name");
 		
-		flexTable.setText(0,1,"Language");
-		flexTable.setText(0,2, "Shopping Cart Size");
-		flexTable.setText(0,3, "Wish List Size");
-		flexTable.setStyleName("centered-table", true);
+		flexTable.addCell(3);
 		
 		for(int i=0; i < users.size(); i++)
 		{
